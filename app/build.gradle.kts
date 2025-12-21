d52bed7291e12d1d940c2d281042d3fe3993c758
Johnny <jhonny317espinoza@gmail.com>
2025-12-21 00:16:57 -0500

    Graddles Configurados y librerias agregadas(ojo copie y pegue exactamente al anterior repositorio puede haber inconsistencias )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,32 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id ("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.lactacare"
-    compileSdk {
-        version = release(36)
-    }
+
+    namespace = "com.example.lactacare.dev"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.lactacare"
+        applicationId = "com.example.lactacare.dev"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
 
     buildTypes {
         release {
@@ -29,21 +38,69 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
+    implementation(libs.androidx.material3)
+    implementation(libs.play.services.cast.framework)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.text)
+    implementation(libs.androidx.core.i18n)
+    // Es mejor usar el BOM de Compose para que todas las versiones coincidan automáticamente
+    val composeBom = platform("androidx.compose:compose-bom:2024.10.00")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.foundation)
+    implementation(libs.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+/// UI Compose (Las versiones se gestionan por el BOM de arriba)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    // Navegación y ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.2")
+
+    // Network / API
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    // Gson para JSON
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+// Security (Para guardar tokens)
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    // Auth & Storage
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    // 5. Iconos extendidos (Para que funcionen Icons.Default.Email, etc.)
+
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+
 }
